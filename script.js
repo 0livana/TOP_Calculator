a79e56e3003a2b71b45637f85b43362d697a204a
Oliva Nana <olivanascript@gmail.com>
2026-08-20 19:17:49 +0100

    feat(style): design the UI of the calculator

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ function divide(a, b) {
     }
     return a / b;
 };
+
+const num1 = 10;
+const num2 = 5;
+const num3 = 0;
+
+//testing the basic functions
+console.log("Addition:", add(num1, num2));  
+console.log("Subtraction:", subtract(num1, num2)); 
+console.log("Multiplication:", multiply(num1, num2)); 
+console.log("Division:", divide(num1, num2));
+
+//next functions:
+function operate(operator, a, b ){
+    if(operator === "+"){
+        return `${a} + ${b} = ${add(a, b)}`;
+    } else if(operator === "-"){
+        return `${a} - ${b} = ${subtract(a, b)}`
+    } else if(operator === "*"){
+        return `${a} * ${b} = ${multiply(a, b)}`
+    } else if(operator === "/"){
+        return `${a} / ${b} = ${divide(a, b)}`
+    } else {
+        return `Wrong operator. Use any of +, -, *, /`
+    }
+}
+
+console.log(operate("*", num1, num2))
+
+//No. 4:
